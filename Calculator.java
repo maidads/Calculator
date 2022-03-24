4ed2126dbc710b16c8fdaba83417207545aeb743
maidads <98819095+maidads@users.noreply.github.com>
2022-03-24 10:56:40 +0100

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -15,6 +15,7 @@ import static java.lang.System.out;
  *   NOTE:
  *   - No negative numbers implemented
  */
+
 public class Calculator {
 
     // Here are the only allowed instance variables!
@@ -51,17 +52,17 @@ public class Calculator {
                 stack.push(pf);
             } else {
                 try {
-                    d1 = Double.valueOf(stack.pop());                               // transform string to a double
-                    d2 = Double.valueOf(stack.pop());                               // pop next ToS
-                    temp = applyOperator(pf,d1,d2);                                 // temp result
-                    stack.push(String.valueOf(temp));                               // put back on stack
+                    d1 = Double.valueOf(stack.pop());                              
+                    d2 = Double.valueOf(stack.pop());                               
+                    temp = applyOperator(pf,d1,d2);                                 
+                    stack.push(String.valueOf(temp));                               
                 }
                 catch (NoSuchElementException e){
                     throw new IllegalArgumentException(MISSING_OPERAND);
                 }
             }
         }
-        return Double.valueOf(stack.pop());                                 // return final result
+        return Double.valueOf(stack.pop());                                 
     }
 
 
@@ -87,7 +88,7 @@ public class Calculator {
     // ------- Infix 2 Postfix --------------------
 
     List<String> infix2Postfix(List<String> infix) {
-        List<String> postfix = new ArrayList<String>();                       // A list
+        List<String> postfix = new ArrayList<String>();                       
         Deque<String> stack = new ArrayDeque<String>();
         String operators = "-+*/^()";
         int countOperators = 0;
@@ -112,14 +113,14 @@ public class Calculator {
                 } else if (infix.get(i).contains("(")) {
                     stack.push(infix.get(i));
                 } else if (getPrecedence(stack.peek()) > getPrecedence(infix.get(i))) {
-                    while (!stack.isEmpty() && getPrecedence(stack.peek()) > getPrecedence(infix.get(i))) {     //pre. stack > pre. infix
+                    while (!stack.isEmpty() && getPrecedence(stack.peek()) > getPrecedence(infix.get(i))) {     
                         postfix.add(stack.pop());
                         countOperators++;
                     }
                     stack.push(infix.get(i));
                 } else if (getPrecedence(stack.peek()) < getPrecedence(infix.get(i))) {
                     stack.push(infix.get(i));
-                } else if (getPrecedence(stack.peek()) == getPrecedence(infix.get(i))) {                // pre. stack = pre. infix
+                } else if (getPrecedence(stack.peek()) == getPrecedence(infix.get(i))) {                
                     if (getAssociativity(infix.get(i)) == Assoc.LEFT) {
                         postfix.add(stack.pop());
                         stack.push(infix.get(i));
@@ -130,7 +131,7 @@ public class Calculator {
                 }
             }
         }
-        while (!stack.isEmpty() && !stack.peek().equals("(")) {        //infix is empty now, so pop out all operators in the stack to postfix
+        while (!stack.isEmpty() && !stack.peek().equals("(")) {        
             postfix.add(stack.pop());
             countOperators++;
         }
@@ -153,7 +154,6 @@ public class Calculator {
     }
 
 
-
     int getPrecedence(String op) {
         if ("+-".contains(op)) {
             return 2;
@@ -166,6 +166,7 @@ public class Calculator {
         }
     }
 
+    
     Assoc getAssociativity(String op) {
         if ("+-*/".contains(op)) {
             return Assoc.LEFT;
@@ -223,174 +224,3 @@ boolean isOperator ( char ch){
             return tokens;
         }
     }
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-    // List String (not char) because numbers (with many chars)
-    List<String> tokenize(String expr) {
-        List<String> tokens = new ArrayList<String>();
-        char[] arr = expr.toCharArray();
-        for (char ch: expr.toCharArray()) {
-            if (Character.isDigit(ch) && expr.contains(" ")) {
-                throw new IllegalArgumentException(MISSING_OPERATOR);
-            }
-            expr = expr.replace(" ", "");
-            String[] split = expr.split("(?<=[\\d.])(?=[^\\d.])|(?<=[^\\d.])(?=[\\d.])|(?=[()])|(?<=[()])");
-            for (String s : split) {
-                // System.out.println(s);
-                tokens.add(s);
-            }
-        }
-            System.out.println(tokens);
-            return tokens;
-        }
-}
-
-
-
-*/
-/*
-
-List<String> tokenize(String expr) {
-    List<String> tokens = new ArrayList<String>();
-    char[] arr = expr.toCharArray();
-    StringBuilder sb = new StringBuilder();
-    StringBuilder sc = new StringBuilder();
-    for (char ch : expr.toCharArray()) {
-        if (!Character.isWhitespace(ch)) {
-            sb.append(ch);
-        }
-    }
-    String s = sb.toString();
-    for (char ch2 : s.toCharArray()) {
-        if (Character.isDigit(ch2)) {
-            sc.append(ch2);
-        } else if (isOperator(ch2)) {
-            if (sc.length() != 0) {
-                tokens.add(sc.toString());
-                sc.setLength(0);
-                sc.append(ch2);
-                tokens.add(sc.toString());
-                sc.setLength(0);
-            } else {
-                sc.append(ch2);
-                tokens.add(sc.toString());
-                sc.setLength(0);
-            }
-        }
-        }
-        if(sc.length()!=0){
-            tokens.add(sc.toString());
-            sc.setLength(0);
-        }
-
-        System.out.println(tokens);
-        return tokens;
-
-    }
-
-    boolean isOperator ( char ch){
-        return (ch == '(' || ch == ')' || ch == '^' || ch == '/' || ch == '*' || ch == '+' || ch == '-');
-    }
-    boolean isOperator ( char ch){
-        return (ch == '(' || ch == ')' || ch == '^' || ch == '/' || ch == '*' || ch == '+' || ch == '-');
-    }
-
-
-    Double evalPostfix(List<String> postfix) {
-        Deque<String> stack = new ArrayDeque<String>();
-        if (postfix.size() == 1) {
-            return Double.valueOf(postfix.get(0));
-        }
-        for (int j = 0; j < postfix.size(); j++) {
-            if (!OPERATORS.contains(postfix.get(j))) {
-                stack.push(postfix.get(j));
-            } else {
-                double x = Double.valueOf(stack.pop());
-                double y = Double.valueOf(stack.pop());
-                stack.push(applyOperator(postfix.get(j), x, y) + "");
-            }
-        }
-        //System.out.println(stack.peek());
-        return Double.valueOf(stack.peek());
-    }
-
-
-
- List<String> tokenize(String expr) {
-        List<String> tokens = new ArrayList<String>();
-        char[] arr = expr.toCharArray();
-        // 10 10 -> sb==10 stöter på mellanslag -> lägg sb i tokens och töm sb
-        StringBuilder sb = new StringBuilder();
-        StringBuilder sc = new StringBuilder();
-        for (char ch : expr.toCharArray()) {
-            if (!Character.isWhitespace(ch)) {
-                sb.append(ch);
-            }
-        }
-        String s = sb.toString();
-        for (char ch2 : s.toCharArray()) {
-            if (Character.isDigit(ch2)) {
-                sc.append(ch2);
-            } else if (isOperator(ch2)) {
-                if (sc.length() != 0) {
-                    tokens.add(sc.toString());
-                    sc.setLength(0);
-                    sc.append(ch2);
-                    tokens.add(sc.toString());
-                    sc.setLength(0);
-                } else {
-                    sc.append(ch2);
-                    tokens.add(sc.toString());
-                    sc.setLength(0);
-                }
-            }
-        }
-        if(sc.length()!=0){
-            tokens.add(sc.toString());
-            sc.setLength(0);
-        }
-
-        System.out.println(tokens);
-        return tokens;
-
-    }
-    boolean isOperator ( char ch){
-        return (ch == '(' || ch == ')' || ch == '^' || ch == '/' || ch == '*' || ch == '+' || ch == '-');
-    }
-
-}
-
-
- */
-/*
-    List<String> tokenize(String expr) {
-        if (noOperator(expr) && expr.contains(" ")) {
-            throw new IllegalArgumentException(MISSING_OPERATOR);
-        }
-        expr = expr.replace(" ", "");
-        List<String> tokens = new ArrayList<String>();                          // Creating a List of type String using ArrayList
-        String[] split = expr.split("(?<=[\\d.])(?=[^\\d.])|(?<=[^\\d.])(?=[\\d.])|(?=[()])|(?<=[()])");
-       for (String s : split) {
-            //System.out.println(s);
-
-            tokens.add(s);
-        }
-       if (!tokens.contains("(") && tokens.contains(")") || tokens.contains("(") && !tokens.contains(")")) {
-           throw new IllegalArgumentException(MISSING_OPERATOR);
-       }
-        //out.println(tokens);
-        return tokens;
-    }
-
- */
